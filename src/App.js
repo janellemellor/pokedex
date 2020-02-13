46a39ceb3c1abc1edd3c3abcae77cf8a6d7b47d7
janelle mellor <janellemellor@gmail.com>
2020-02-12 16:41:30 -0800

    add component files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header.js';
+import PokeList from './PokeList.js';
+import PokeItem from './PokeItem.js';
+
+
+class App extends Component {
+  
+  render()
+  return (
+   
+  );
+}
+
+export default App;
