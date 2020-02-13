ed6b17450cec66d22cef0e05ddf52ebd4c2138ae
janelle mellor <janellemellor@gmail.com>
2020-02-12 19:54:55 -0800

    add tests for components

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import App from './App.js';
+import Header from './Header.js';
+import PokeList from './PokeList.js';
+import PokeItem from './PokeItem.js';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App.js correctly', () => {
+  const tree = renderer
+    .create(<App />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('renders Header.js correctly', () => {
+  const tree = renderer
+  .create(<Header />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('renders PokeList.js correctly', () => {
+  const tree = renderer
+  .create(<PokeList pokemon={this.state.pokemon} />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('renders PokeItem.js correctly', () => {
+  const tree = renderer
+  .create(<PokeItem />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- src/PokeList.js
+++ src/PokeList.js
@@ -4,7 +4,7 @@ import PokeItem from './PokeItem.js';
 export default class PokeList extends Component {
     render() {
         return <ul>
-            {this.props.pokemon.map(poke => <PokeItem title={poke.pokemon} img={poke.url_image} type1={poke.type_1}type2={poke.type_2} ability1={poke.ability_1} ability2={poke.ability_2}/>
+            {this.props.pokemon.map(poke => <PokeItem title={poke.pokemon} img={poke.url_image} type1={poke.type_1}type2={poke.type_2} ability1={poke.ability_1} ability2={poke.ability_2} key={poke.id}/>
             )}
 
         </ul>
